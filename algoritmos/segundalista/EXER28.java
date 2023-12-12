7c0072ca8c936b655d13d47f83862eab61b4219e
Edenilson Fabiano <20231ITA01GB0011@alunos.ifbaiano.edu.br>
2023-12-11 23:49:05 -0300

    Update EXER28.java

--- algoritmos/segundalista/EXER28.java
+++ algoritmos/segundalista/EXER28.java
@@ -1,31 +1,27 @@
-
-
 import javax.swing.JOptionPane;
 
 public class EXER28 {
+  
     public static void main(String[] args) {
 
-        /*
-            Ler o valor de um cheque e escrever o quanto vai ser recolhido de CPMF. Considere que
-            o imposto recolhe um taxa de 0,38%
-         */
-
-
+        //Ler o valor de um cheque e escrever o quanto vai ser recolhido de CPMF. Considere que o imposto recolhe um taxa de 0,38%
+        
         //declarar variáveis
         double cheque, CPMF, chequeDesc;
         String stCheque;
 
         //entrada de dados
         stCheque = JOptionPane.showInputDialog(null, "Iforme o valor do CHEQUE:");
+        
         cheque = Double.parseDouble(stCheque);
 
         //cálculo valor de desconto do CPMF
         CPMF = cheque * 0.0038;
+        
         chequeDesc = cheque - CPMF;
 
         //saída de dados
-        JOptionPane.showMessageDialog(null, "Valor cheque: R$" + cheque + "\n" +
-                "CPMF: R$" + CPMF + "\n" + "Valor atualizado: R$" + chequeDesc);
+        JOptionPane.showMessageDialog(null, "Valor cheque: R$" + cheque + "\n" + "CPMF: R$" + CPMF + "\n" + "Valor atualizado: R$" + chequeDesc);
 
     }
 }
